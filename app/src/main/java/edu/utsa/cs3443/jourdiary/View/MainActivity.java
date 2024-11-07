5866b06221c3564a829016228a4d2df02a6a3dec
Maya Motaghi <mahya.motaghi02@gmail.com>
2024-11-07 15:36:16 -0600

    User Entry

--- app/src/main/java/edu/utsa/cs3443/jourdiary/View/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/jourdiary/View/MainActivity.java
@@ -1,4 +1,4 @@
-package edu.utsa.cs3443.jourdiary;
+package edu.utsa.cs3443.jourdiary.View;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import edu.utsa.cs3443.jourdiary.R;
 
 public class MainActivity extends AppCompatActivity {
 
