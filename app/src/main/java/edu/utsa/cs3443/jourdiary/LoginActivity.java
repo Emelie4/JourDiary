592ea9ea64425b0db47be5683d7e58c93a0665f7
Emelie4 <108645700+Emelie4@users.noreply.github.com>
2024-11-19 19:51:35 -0600

    Login and sign up page. Functionality

--- app/src/main/java/edu/utsa/cs3443/jourdiary/LoginActivity.java
+++ app/src/main/java/edu/utsa/cs3443/jourdiary/LoginActivity.java
@@ -1,4 +1,12 @@
 package edu.utsa.cs3443.jourdiary;
 
-public class LoginActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
