df6703302474e8d891af1a3d280e3a55a01c5c39
AlecCaesar <agomeyac@yahoo.com>
2024-12-02 23:36:40 -0600

    Update LoginActivity.java

--- app/src/main/java/edu/utsa/cs3443/jourdiary/LoginActivity.java
+++ app/src/main/java/edu/utsa/cs3443/jourdiary/LoginActivity.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+* Represents the Login page in the application.
+* @author Team Skittlez
+*/
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Button
         Button buttonLoginSubmit = findViewById(R.id.buttonLoginSubmit);
 
+        //Brings User to Diary Log
         buttonLoginSubmit.setOnClickListener(v -> {
             Intent intent = new Intent(LoginActivity.this, DiaryLogActivity.class);
             startActivity(intent);
