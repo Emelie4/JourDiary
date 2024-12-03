6a8f554ef017bc61eae2a02ece46b31bb58f539f
AlecCaesar <agomeyac@yahoo.com>
2024-12-02 23:40:31 -0600

    Update NewDiary.java

--- app/src/main/java/edu/utsa/cs3443/jourdiary/NewDiary.java
+++ app/src/main/java/edu/utsa/cs3443/jourdiary/NewDiary.java
@@ -13,21 +13,28 @@ import java.util.concurrent.Executors;
 import edu.utsa.cs3443.jourdiary.database.Database;
 import edu.utsa.cs3443.jourdiary.Model.DiaryEntry;
 
+/**
+* Represents the New Diary Page in the application.
+* @author Team Skittlez
+*/
+
 public class NewDiary extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_diary);
 
+        //Text and Buttons
         EditText diaryContent = findViewById(R.id.editText);
         Button saveButton = findViewById(R.id.saveButton);
         Button prevButton = findViewById(R.id.prevButton);
 
+        //Saves Diary Entry
         saveButton.setOnClickListener(v -> {
             String content = diaryContent.getText().toString();
             if (!content.isEmpty()) {
                 String currentDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
-
+                //Creates Diary Entry
                 DiaryEntry diaryEntry = new DiaryEntry(currentDate, content);
                 Executors.newSingleThreadExecutor().execute(() -> {
                     Database.getInstance(this).diaryEntryDao().insert(diaryEntry);
@@ -37,6 +44,7 @@ public class NewDiary extends AppCompatActivity {
             }
         });
 
+        //Brings User back to Diary Log
         prevButton.setOnClickListener(v -> {
             Intent intent = new Intent(NewDiary.this, DiaryLogActivity.class);
             startActivity(intent);
