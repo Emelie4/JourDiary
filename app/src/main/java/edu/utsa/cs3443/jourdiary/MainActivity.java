2bcc7dda227fa893f85fbf16af71b7013340903d
AlecCaesar <agomeyac@yahoo.com>
2024-12-02 23:38:30 -0600

    Update MainActivity.java

--- app/src/main/java/edu/utsa/cs3443/jourdiary/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/jourdiary/MainActivity.java
@@ -5,26 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.widget.Button;
 
+/**
+* Represents the main activity in the application.
+* @author Team Skittlez
+*/
+
 public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Buttons
         Button loginButton = findViewById(R.id.buttonLogin);
         Button signUpButton = findViewById(R.id.buttonSignUp);
         Button aboutButton = findViewById(R.id.buttonAbout);
 
+        //Brings User to Login Page
         loginButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
         });
 
+        //Brings User to Sign Up Page
         signUpButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
             startActivity(intent);
         });
 
+        //Brings User to About Page
         aboutButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, AboutActivity.class);
             startActivity(intent);
