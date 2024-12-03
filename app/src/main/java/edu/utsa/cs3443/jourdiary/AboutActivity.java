48bbb7658093ca750f2b5deb7d74beb7c6a906a0
AlecCaesar <agomeyac@yahoo.com>
2024-12-02 23:31:11 -0600

    Update AboutActivity.java

--- app/src/main/java/edu/utsa/cs3443/jourdiary/AboutActivity.java
+++ app/src/main/java/edu/utsa/cs3443/jourdiary/AboutActivity.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.widget.Button;
 
+/**
+* Represents the About page in the application.
+* @author Team Skittlez
+*/
+
 public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        //Button
         Button prevButton = findViewById(R.id.prevButton);
 
+        //Brings User back to Main Menu
         prevButton.setOnClickListener(v -> {
             Intent intent = new Intent(AboutActivity.this, MainActivity.class);
             startActivity(intent);
