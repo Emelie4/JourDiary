c056a2f693ed4e2830f890fe8686bb312175aaf4
AlecCaesar <agomeyac@yahoo.com>
2024-12-02 23:22:37 -0600

    Update DiaryEntry.java

--- app/src/main/java/edu/utsa/cs3443/jourdiary/Model/DiaryEntry.java
+++ app/src/main/java/edu/utsa/cs3443/jourdiary/Model/DiaryEntry.java
@@ -3,13 +3,23 @@ package edu.utsa.cs3443.jourdiary.Model;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+/**
+* Represents a diary entry in the database.
+* @author Team Skittlez
+*/
+
 @Entity(tableName = "diary_entries")
 public class DiaryEntry {
     @PrimaryKey(autoGenerate = true)
     public int id;
-
     public String date;
     public String content;
+
+    /**
+    * Constructor for DiaryEntry
+    * @param date
+    * @param content
+    */
 
     public DiaryEntry(String date, String content) {
         this.date = date;
