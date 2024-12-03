c6f0db8cde560af5052a9f50c2ed70e6aa77dc40
AlecCaesar <agomeyac@yahoo.com>
2024-12-02 23:25:53 -0600

    Update User.java

--- app/src/main/java/edu/utsa/cs3443/jourdiary/Model/User.java
+++ app/src/main/java/edu/utsa/cs3443/jourdiary/Model/User.java
@@ -1,11 +1,23 @@
 package edu.utsa.cs3443.jourdiary.Model;
 
+/**
+* Represents a user in the application
+* @author Team Skittlez
+*/
 public class User {
     private String firstName;
     private String lastName;
     private String userName;
     private String passWord;
 
+    /**
+    * Constructor for User
+    * @param firstName
+    * @param lastName
+    * @param userName
+    * @param passWord
+    */
+    
     public User(String firstName, String lastName, String userName, String passWord) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -13,6 +25,7 @@ public class User {
         this.passWord = passWord;
     }
 
+    //Getters and Setters
     public String getFirstName() {
         return firstName;
     }
@@ -45,6 +58,7 @@ public class User {
         this.passWord = passWord;
     }
 
+    //toString Method
     @Override
     public String toString() {
         return firstName + " " + lastName + " " + userName + " " + passWord;
