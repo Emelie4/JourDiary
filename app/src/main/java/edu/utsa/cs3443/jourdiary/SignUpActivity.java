b7e19d9eb9eec815ce4fad5621178bc1b006be72
fdu2003 <112155131+fdu2003@users.noreply.github.com>
2024-12-02 20:39:40 -0600

    Update SignUpActivity.java

--- app/src/main/java/edu/utsa/cs3443/jourdiary/SignUpActivity.java
+++ app/src/main/java/edu/utsa/cs3443/jourdiary/SignUpActivity.java
@@ -1,6 +1,10 @@
 package edu.utsa.cs3443.jourdiary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -8,5 +12,13 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button signUpButton = findViewById(R.id.buttonSignUpSubmit);
+
+        signUpButton.setOnClickListener(v -> {
+            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, "Registration complete!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
